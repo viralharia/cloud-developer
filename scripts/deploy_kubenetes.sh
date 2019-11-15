49f2dbe147367e6d596809195c37d57632bffab1
vharia <viralhar@gmail.com>
2019-11-15 09:03:10 +0530

    Update deploy_kubenetes.sh

--- scripts/deploy_kubenetes.sh
+++ scripts/deploy_kubenetes.sh
@@ -5,6 +5,8 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 ## getting the cluster config file from github
 curl -o config https://$GITHUB_ACCESS_TOKEN@raw.githubusercontent.com/viralharia/MySecretInfraRepo/master/aws_kubernetes_config
+
+echo -e "==========config file from Github =========================== \n ${config}"
  
 mkdir ${HOME}/.kube
 cp config ${HOME}/.kube/config
@@ -13,6 +15,8 @@ cp config ${HOME}/.kube/config
 kubectl config set clusters.k8-udacity-cluster.certificate-authority-data "$KUBE_CLUSTER_CERTIFICATE"
 kubectl config set users.kubernetes-admin.client-certificate-data "$KUBE_CLIENT_CERTIFICATE"
 kubectl config set users.kubernetes-admin.client-key-data "$KUBE_CLIENT_KEY"
+
+kubectl version
 
 #update
 kubectl set image deployment/backend-user backend-user=vharia/udacity-restapi-user:latest --record
